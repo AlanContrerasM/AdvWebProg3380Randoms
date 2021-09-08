7d400f11a48bd56c9ed07eb9cf1ecb31206aa34a
Alan Contreras <alanrcontrerasm@gmail.com>
2021-09-08 11:37:16 -0700

    Finish ToDoList, add filter, add, and remove

--- HandsOn_Exercises/toDoList.js
+++ HandsOn_Exercises/toDoList.js
@@ -7,15 +7,29 @@
 </ul> */
 
 let tasks = document.getElementById("tasks");
+let filter = document.getElementById("filter");
+let tasksList = [];
+
+
+//create an Array from the current tasks, so we can manipulate it.
+for(let li of tasks.children){
+    tasksList.push(li.textContent.slice(0,li.textContent.length - 2));
+}
+console.log(tasksList);
 
 
 //delete children from tasks list
 
 function removeTask(element){
-    console.log("clicked " + element);
+    console.log(element.parentNode.parentNode.children);
     //let index = Array.from(element.parentNode.parentNode.children).indexOf(element.parentNode);
     //console.log(index);
     tasks.removeChild(element.parentNode);
+
+    var index = 0;
+    while( element.parentNode.previousSibling != null ){index++;}
+  
+    tasksList.splice(index ,1);
 }
 
 
@@ -49,10 +63,15 @@ addForm.addEventListener("submit", (e) => {
 
     let li = document.createElement("li");
     li.classList.add("list-group-item");
-    li.textContent = document.getElementById("task").value;
+    li.textContent = document.getElementById("task").value + " ";
 
     li.appendChild(button);
     tasks.appendChild(li);
+
+    tasksList.push(document.getElementById("task").value )
+
+    //clear input field
+    document.getElementById("task").value = "";
     
   });
 
@@ -60,3 +79,34 @@ addForm.addEventListener("submit", (e) => {
 
 //filter tasks
 //<input type="text" class="form-control" id="filter" placeholder="Search Tasks...">
+filter.addEventListener("input", e =>{
+
+    //filter tasks array with search parameter
+    let foundTasks = tasksList.filter(task=>{
+        return task.includes(e.target.value);
+    })
+
+    //clear current tasks Dom list
+    tasks.innerHTML = "";
+
+    //create new DOM list with filtered arraylist
+    foundTasks.forEach(task=>{
+        let button = document.createElement("button");
+
+        button.classList.add("btn", "btn-danger", "btn-sm", "float-right", "delete");
+        button.textContent = "X";
+        button.addEventListener("click", ()=> removeTask(button));
+    
+        let li = document.createElement("li");
+        li.classList.add("list-group-item");
+        li.textContent = task + " ";
+    
+        li.appendChild(button);
+        tasks.appendChild(li);
+    })
+   
+
+
+
+})
+
