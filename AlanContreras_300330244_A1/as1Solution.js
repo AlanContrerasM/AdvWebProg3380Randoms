8fcbec13116aff175dedaaed63b593228c0b6ffc
Alan Contreras <alanrcontrerasm@gmail.com>
2021-09-15 15:22:57 -0700

    Complete 4 ex assg1

--- AlanContreras_300330244_A1/as1Solution.js
+++ AlanContreras_300330244_A1/as1Solution.js
@@ -184,13 +184,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
    minutes.addEventListener('blur', (e)=>{
       let mins = e.target.value;
-
       let hours = Math.floor(mins/60);
       let extraMins = mins % 60;
-      console.log(hours, extraMins);
-
+      //console.log(hours, extraMins);
       document.getElementById('amount').value = (hours*40 + extraMins) * selectBox.options.length;
-
    })
 
 
@@ -198,15 +195,100 @@ document.addEventListener("DOMContentLoaded", function(event) {
     * Solutionn 3 -- Changing Quotes
     * 
     */
+   let quotes = [
+      "Life isn't about finding yourself. Life is about creating yourself. - George Bernard Shaw",
+      "Do what you can, with what you have, where you are. - Theodore Roosevelt",
+      "There is no greater agony than bearing an untold story inside you. - Maya Angelou",
+      "I have not failed. I've just found 10,000 ways that won't work. - Thomas A. Edison",
+      "We accept the love we think we deserve. - Stephen Chbosky",
+      "Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi",
+      "No one can make you feel inferior without your consent. - Eleanor Roosevelt",
+      "Be yourself; everyone else is already taken. - Oscar Wilde",
+      "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston S. Churchill",
+      "If my life is going to mean anything, I have to live it myself. - Rick Riordan",
+      "Isn't it nice to think that tomorrow is a new day with no mistakes in it yet? - L.M. Montgomery",
+      "You can't stay in your corner of the Forest waiting for others to come to you. You have to go to them sometimes. - A.A. Milne",
+      "Pain is inevitable. Suffering is optional. - Haruki Murakami",
+      "Always do what you are afraid to do. - Ralph Waldo Emerson",
+      "Our lives begin to end the day we become silent about things that matter. - Martin Luther King Jr.",
+      "In the end, we will remember not the words of our enemies, but the silence of our friends. - Martin Luther King Jr.",
+      "Talent hits a target no one else can hit. Genius hits a target no one else can see. - Arthur Schopenhauer",
+      "The mind is its own place, and in itself can make a heaven of hell, a hell of heaven.. - John Milton",
+      "Fantasy is hardly an escape from reality. It's a way of understanding it. - Lloyd Alexander",
+      "Waiting is painful. Forgetting is painful. But not knowing which to do is the worst kind of suffering. - Paulo Coelho",
+      "Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson",
+      "I can be changed by what happens to me. But I refuse to be reduced by it. - Maya Angelou",
+      "Never let your sense of morals prevent you from doing what is right. - Isaac Asimov",
+      "Hell is empty and all the devils are here. - William Shakespeare",
+      "Turn your wounds into wisdom. - Oprah Winfrey"      
+    ]
+
+   function DisplayQuotes(){
+   //creates 4 random numbers between 0 and 24,
+   let randoms = [];
+
+   for(let i = 0; i < 4; i++){
+      let ran = Math.floor(Math.random()* quotes.length);
+      let duplicate = false;
+
+      //check for duplicates
+      randoms.forEach(a =>{
+         a == ran ? duplicate = true : null;
+      })
+
+      duplicate ? i-- : randoms.push(ran);
+   }
+   
+   let displayQ = document.getElementById('displayQuotes');
+   displayQ.textContent = "";
+   
+   randoms.forEach((a) =>{
+      displayQ.innerHTML += quotes[a] + "<br/>";
+   })
+   
 
 
+   
+   }
+   //create interval
+   let myInterval = setInterval(DisplayQuotes, 5000);
+   //clearInterval(myInterval);
+   
 
 
    /**
     * Solution 4 -- Animated Images
     * 
     */
+   let imgSrc = [
+      "images/img0.png", "images/img1.png", "images/img2.png",
+      "images/img3.png", "images/img4.png", "images/img5.png",
+      "images/img6.png", "images/img7.png", "images/img8.png",
+      "images/img9.png", "images/img10.png", "images/img11.png",
+      "images/img12.png", "images/img13.png"
+   ]
 
+   let imgArray = new Array();
+   //caching images into an array
+   imgSrc.forEach(src=>{
+      let img = new Image();
+      img.src = src;
+      img.id = 'animateImage';
+      img.alt = 'Animate Images.';
+      imgArray.push(img);
+   })
+
+   let next = 0;
+   function AnimateImages(){
+      let animateImgFrame = document.getElementById('animateImage');
+
+      animateImgFrame.parentNode.replaceChild(imgArray[next], animateImgFrame);
+      next ==13? next = 0 : next++;
+   }
+
+   //animate it
+   let myImageAnimation = setInterval(AnimateImages, 150);
+   //clearInterval(myImageAnimation);
 
 
 
